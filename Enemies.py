21812a0d0621213278376d73beb397a4cc7b84f2
Philip Smith <philip@Alans-Mac-mini.local>
2018-12-22 14:24:15 -0500

    added self and pygame

--- Enemies.py
+++ Enemies.py
@@ -1,6 +1,7 @@
 from sprite import Sprite
+import pygame
 
 
 class enemy(Sprite):
-    def __init__(x, y, width, height, path):
+    def __init__(self, x, y, width, height, path):
         super().__init__(x, y, height, width)
