2e0bc14acbeb61a22b388653db6e53b161ab3b51
Nithya G <nbgolla@gmail.com>
2018-12-31 15:49:03 -0500

    Fixed bug and added unit test

--- elliptical.py
+++ elliptical.py
@@ -29,11 +29,22 @@ class Elliptical(object):
     def __init__(self):
         self.num_rotations = 0
         self.speed_RPM = 0
+        self.thread = None
         if hardware:
-            threading.Thread(target=self._hardware_loop()).start()
+            self.thread = threading.Thread(target=self._hardware_loop)
+            self.thread.stopped = False
+            self.thread.start() 
 
     def get_speed(self):
         return self.speed_RPM
 
     def get_distance(self):
         return self.num_rotations * DIST_PER_ROTATION_M
+
+if __name__ == "__main__":
+   elliptical = Elliptical()
+   print("Init finished")
+   while True:
+      print("Speed: {0} Distance {1}".format(int(elliptical.get_speed()), int(elliptical.get_distance())))
+      time.sleep(1)
+
