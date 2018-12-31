1fa8d1ef1d55cb6fb492b1f04164f57bccdef6b9
Philip Smith <philip@Alans-Mac-mini.local>
2018-12-31 15:16:53 -0500

    refactored

--- elliptical.py
+++ elliptical.py
@@ -1,37 +1,39 @@
 import time
+import threading
+
 try:
     import explorerhat
+    hardware = True
 except:
-    pass
+    hardware = False
 
-DIST_PER_ROTATION = (2.0 * 3.1415 * 19.0) / 100.0
+DIST_PER_ROTATION_M = (2.0 * 3.1415 * 19.0) / 100.0
 
 
-class elliptical(object):
-    def __init__(self):
-        self.num_rotations = 0
-        self.distance_m = 0
-        self.speed_RPM = 0
-
-    def read_sensor():
+class Elliptical(object):
+    def _read_sensor():
         return explorerhat.input.three.read()
 
-    def get_value(self):
-        try:
-            while True:
-                start_time = time.time()
-                while read_sensor():
-                    pass
-                while read_sensor() == False:
-                    pass
-                end_time = time.time()
-                diff = end_time - start_time
-                self.speed_RPM = (60 / diff)
-                self.num_rotations += 1
-                self.distance_m = num_rotations * DIST_PER_ROTATION_M
-                return ([self.speed_RPM, self.distance_m])
-                print("{0} RPM - {1} m".format(
-                    int(speed_RPM), self.distance_m))
+    def _hardware_loop(self):
+        while True:
+            start_time = time.time()
+            while _read_sensor():
+                pass
+            while _read_sensor() == False:
+                pass
+            end_time = time.time()
+            diff = end_time - start_time
+            self.speed_RPM = (60 / diff)
+            self.num_rotations += 1
 
-        except:
-            return ([321, 321])
+    def __init__(self):
+        self.num_rotations = 0
+        self.speed_RPM = 0
+        if hardware:
+            threading.Thread(target=self._hardware_loop()).start()
+
+    def get_speed(self):
+        return self.speed_RPM
+
+    def get_distance(self):
+        return self.num_rotations * DIST_PER_ROTATION_M
